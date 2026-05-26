bc2ae9531881a556d64a4631f6603fcffc65a60e
christianksimmons04 <christianksimmons04@gmail.com>
2026-05-25 22:33:08 -0500

    Update HideAndSeekGame.py

--- HideAndSeekGame.py
+++ HideAndSeekGame.py
@@ -9,10 +9,20 @@ import random
 countdown = 11
 seeking_time = 0
 
+# Hiding spots
+set_one = ["Couch", "Chair", "TV"]
+set_two = ["Table", "Recliner", "Curtain"]
+set_three = ["Shower", "Closet", "Chase"]
+
+# Set random spot
+random_option_one = random.choice(set_one)
+random_option_two = random.choice(set_two)
+random_option_three = random.choice(set_three)
+
 # Starting the game
 print("===Enter the Number===\n")
 
-print("[1] Couch | [2] Curtain | [3] Shower\n")
+print(f"[1] {random_option_one} | [2] {random_option_two} | [3] {random_option_three}\n")
 
 user_option = input("Pick a hiding spot!\n")
 
@@ -24,7 +34,7 @@ elif user_option == 2:
 elif user_option == 3:
     print("[You chose the shower!]\n")
 else:
-    print("Invalid integer chosen! Choosing for you.\n")
+    print("Inavlid ineger chosen! Choosing for you.\n")
     user_option = random.randint(1, 3)
     print(f"You chose: {user_option}\n")
 
